34284dcd8c9679bd2d7e449436d17bb4ae560709
Gumaro P <167045860+gpm209@users.noreply.github.com>
2026-04-21 20:03:22 -0700

    useState implemented, TodoList props added, and a new TodoListItem component implement

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,14 +1,10 @@
-function TodoList() {
-  const todoList = [
-    { id: 1, title: "Go over lesson" },
-    { id: 2, title: "Go to group session" },
-    { id: 3, title: "Work on assignments" },
-  ];
+import TodoListItem from "./TodoListItem.jsx";
 
+function TodoList({ todoList }) {
   return (
     <ul>
       {todoList.map((todo) => (
-        <li key={todo.id}>{todo.title}</li>
+        <TodoListItem key={todo.id} todo={todo} />
       ))}
     </ul>
   );
